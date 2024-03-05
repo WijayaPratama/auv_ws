889c49ad9295c1ce74c1561e6fca996c9b89a620
arya <pratamawijaya549@gmail.com>
2024-03-05 19:05:59 +0700

    first commit

---
+++ scripts/node_control.py
@@ -0,0 +1,171 @@
+#!/usr/bin/env python3
+
+import rospy
+import time
+from simple_pid import PID
+from std_msgs.msg import Bool, String
+from robotic_sas_auv_ros.msg import Error
+from robotic_sas_auv_ros.msg import Actuator
+
+class Movement():
+    def _init_(self):
+        self.pwm_actuator = Actuator()
+        self.pwm_actuator.thruster_1 = 1500
+        self.pwm_actuator.thruster_2 = 1500
+        self.pwm_actuator.thruster_3 = 1500
+        self.pwm_actuator.thruster_4 = 1500
+        self.pwm_actuator.thruster_5 = 1500
+        self.pwm_actuator.thruster_6 = 1500
+        self.pwm_actuator.thruster_7 = 1500
+        self.pwm_actuator.thruster_8 = 1500
+
+        self.pub_pwm_actuator = rospy.Publisher('pwm_actuator', Actuator, queue_size=10)
+
+    def surgeSwayYaw(self, pwm_thruster_1, pwm_thruster_2, pwm_thruster_3, pwm_thruster_4):
+        self.pwm_actuator.thruster_1 = pwm_thruster_1
+        self.pwm_actuator.thruster_2 = pwm_thruster_2
+        self.pwm_actuator.thruster_3 = pwm_thruster_3
+        self.pwm_actuator.thruster_4 = pwm_thruster_4
+
+    def heaveRollPitch(self, pwm_thruster_5, pwm_thruster_6, pwm_thruster_7, pwm_thruster_8):
+        self.pwm_actuator.thruster_5 = pwm_thruster_5
+        self.pwm_actuator.thruster_6 = pwm_thruster_6
+        self.pwm_actuator.thruster_7 = pwm_thruster_7
+        self.pwm_actuator.thruster_8 = pwm_thruster_8
+
+    def surge(self, pwm):
+        self.pwm_actuator.thruster_1 = 1500 + pwm
+        self.pwm_actuator.thruster_2 = 1500 + pwm
+        self.pwm_actuator.thruster_3 = 1500 + pwm
+        self.pwm_actuator.thruster_4 = 1500 + pwm
+
+    def sway(self, pwm):
+        self.pwm_actuator.thruster_1 = 1500 - pwm
+        self.pwm_actuator.thruster_2 = 1500 + pwm
+        self.pwm_actuator.thruster_3 = 1500 + pwm
+        self.pwm_actuator.thruster_4 = 1500 - pwm
+
+    def yaw(self, pwm):
+        self.pwm_actuator.thruster_1 = 1500 - pwm
+        self.pwm_actuator.thruster_2 = 1500 + pwm
+        self.pwm_actuator.thruster_3 = 1500 - pwm
+        self.pwm_actuator.thruster_4 = 1500 + pwm
+
+    def heave(self, pwm):
+        self.pwm_actuator.thruster_5 = 1500 + pwm
+        self.pwm_actuator.thruster_6 = 1500 + pwm
+        self.pwm_actuator.thruster_7 = 1500 - pwm
+        self.pwm_actuator.thruster_8 = 1500 - pwm
+
+    def roll(self, pwm):
+        self.pwm_actuator.thruster_5 = 1500 - pwm
+        self.pwm_actuator.thruster_6 = 1500 + pwm
+        self.pwm_actuator.thruster_7 = 1500 + pwm
+        self.pwm_actuator.thruster_8 = 1500 - pwm
+
+    def pitch(self, pwm):
+        self.pwm_actuator.thruster_5 = 1500 + pwm
+        self.pwm_actuator.thruster_6 = 1500 + pwm
+        self.pwm_actuator.thruster_7 = 1500 + pwm
+        self.pwm_actuator.thruster_8 = 1500 + pwm
+
+    def stop(self):
+        self.pwm_actuator.thruster_1 = 1500
+        self.pwm_actuator.thruster_2 = 1500
+        self.pwm_actuator.thruster_3 = 1500
+        self.pwm_actuator.thruster_4 = 1500
+        self.pwm_actuator.thruster_5 = 1500
+        self.pwm_actuator.thruster_6 = 1500
+        self.pwm_actuator.thruster_7 = 1500
+        self.pwm_actuator.thruster_8 = 1500
+
+    def publish(self):
+        print('Publish PWM', self.pwm_actuator)
+        self.pub_pwm_actuator.publish(self.pwm_actuator)
+
+class Subscriber():
+    def _init_(self):
+        self.movement = Movement()
+        self.movement.stop()
+
+        self.pid_heave = PID(1000, 50, 200)
+        self.pid_roll = PID(200, 2, 3)
+        self.pid_pitch = PID(200, 0, 1)
+        self.pid_yaw = PID(200, 0, 0)
+
+        self.pwm_roll = 0
+        self.pwm_pitch = 0
+        self.pwm_yaw = 0
+
+        self.pwm_surge = 0
+        self.pwm_sway = 0
+        self.pwm_heave = 0
+
+        self.rate = rospy.Rate(10)
+
+        # Subscriber
+        rospy.Subscriber('error', Error, self.callback_error)
+        rospy.Subscriber('is_start', Bool, self.callback_is_start)
+        rospy.Subscriber('movement', String, self.callback_movement)
+    
+    def constrain(self, value):
+        return min(max(value, 1250), 1750)
+
+    def surgeSwayYaw(self):
+        pwm_thruster_1 = self.constrain(1500 + self.pwm_surge + self.pwm_sway + self.pwm_yaw)
+        pwm_thruster_2 = self.constrain(1500 + self.pwm_surge - self.pwm_sway - self.pwm_yaw)
+        pwm_thruster_3 = self.constrain(1500 + self.pwm_surge - self.pwm_sway + self.pwm_yaw)
+        pwm_thruster_4 = self.constrain(1500 + self.pwm_surge + self.pwm_sway - self.pwm_yaw)
+        self.movement.surgeSwayYaw(pwm_thruster_1, pwm_thruster_2, pwm_thruster_3, pwm_thruster_4)
+
+    def heaveRollPitch(self):
+        pwm_thruster_5 = self.constrain(1500 + self.pwm_heave + self.pwm_roll - self.pwm_pitch)
+        pwm_thruster_6 = self.constrain(1500 + self.pwm_heave - self.pwm_roll - self.pwm_pitch)
+        pwm_thruster_7 = self.constrain(1500 - self.pwm_heave - self.pwm_roll - self.pwm_pitch)
+        pwm_thruster_8 = self.constrain(1500 - self.pwm_heave + self.pwm_roll - self.pwm_pitch)
+        self.movement.heaveRollPitch(pwm_thruster_5, pwm_thruster_6, pwm_thruster_7, pwm_thruster_8)
+
+    def stabilize_roll(self, error):
+        self.pwm_roll = self.pid_roll(error)
+
+    def stabilize_pitch(self, error):
+        self.pwm_pitch = self.pid_pitch(error)
+
+    def stabilize_yaw(self, error):
+        self.pwm_yaw = self.pid_yaw(error)
+
+    def stabilize_depth(self, error):
+        self.pwm_heave = self.pid_heave(error)
+
+    def callback_movement(self, data):
+        if data.data == 'SURGE':
+            self.pwm_surge = 200
+
+    def callback_error(self, data):
+        self.stabilize_roll(data.roll)
+        self.stabilize_pitch(data.pitch)
+        self.stabilize_depth(data.depth)
+        self.stabilize_yaw(data.yaw)
+
+    def callback_is_start(self, data):
+        if data.data:
+            self.heaveRollPitch()
+        else:
+            self.movement.stop()
+
+        self.movement.publish()
+
+        self.rate.sleep()
+
+    def spin(self):
+        rospy.spin()
+
+def main():
+    rospy.init_node('node_control', anonymous=True)
+
+    subscriber = Subscriber()
+
+    subscriber.spin()
+
+if __name__ == '_main_':
+    main()
---
+++ scripts/node_navigation.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import Bool, String
+from robotic_sas_auv_ros.msg import Sensor, SetPoint, Error
+
+class Subscriber():
+    def _init_(self):
+        self.error = Error()
+        self.set_point = SetPoint()
+
+        self.rate = rospy.Rate(10)
+
+        # Publisher
+        self.pub_error = rospy.Publisher('error', Error, queue_size=10)
+        self.pub_movement = rospy.Publisher('movement', String, queue_size=10)
+
+        # Subscriber
+        rospy.Subscriber('set_point', SetPoint, self.callback_set_point)
+        rospy.Subscriber('sensor', Sensor, self.callback_sensor)
+        rospy.Subscriber('is_start', Bool, self.callback_is_start)
+
+    def generate_is_stable(self, thresh, error):
+        return -(thresh) <= error < thresh
+
+    def calculate_orientation_error(self, current, target):
+        error = (target - current) % 2
+        if error > 1:
+            error -= 2
+        return error
+
+    def calculate_compass_error(self, current, target):
+        return (target - current + 180) % 360 - 180
+
+    def callback_set_point(self, data):
+        self.set_point = data
+
+    def callback_sensor(self, data):
+        # Calculate Error Value
+        error_roll = self.calculate_orientation_error(data.x, self.set_point.roll)
+        error_pitch = self.calculate_orientation_error(data.y, self.set_point.pitch)
+        error_yaw = self.calculate_orientation_error(data.z, self.set_point.yaw)
+        error_depth = self.set_point.depth - data.depth
+
+        # Determine Stable Position
+        is_stable_roll = self.generate_is_stable(0.1, error_roll)
+        is_stable_pitch = self.generate_is_stable(0.1, error_pitch)
+        is_stable_yaw = self.generate_is_stable(0.1, error_yaw)
+        is_stable_depth = self.generate_is_stable(0.05, error_depth)
+
+        # Validate Error Value
+        self.error.roll = 0 if is_stable_roll else error_roll
+        self.error.pitch = 0 if is_stable_pitch else error_pitch
+        self.error.yaw = 0 if is_stable_yaw else error_yaw
+        self.error.depth = 0 if is_stable_depth else error_depth
+
+    def callback_is_start(self, data):
+        if data.data:
+            self.pub_error.publish(self.error)
+        
+        self.rate.sleep()
+
+    def spin(self):
+        rospy.spin()
+
+def main():
+    rospy.init_node('node_navigation', anonymous=True)
+
+    subscriber = Subscriber()
+
+    subscriber.spin()
+
+if __name__ == '_main_':
+    main()
